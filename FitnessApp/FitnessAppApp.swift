698cc3fa6ec7a7073d97a5a5844f8f1ab75458a4
farz1212 <farzaad1212@gmail.com>
2024-10-13 16:54:36 -0400

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Farzaad Goiporia on 2024-10-13.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
